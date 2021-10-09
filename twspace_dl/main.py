a0cde8b00393e6653e48ffa4fb4caa60c0451b87
Ryu18 <ryu@tpgjbo.xyz>
2021-10-09 11:30:19 +0200

    added logging

--- twspace_dl/main.py
+++ twspace_dl/main.py
@@ -1,10 +1,13 @@
 """Script designed to help download twitter spaces"""
 import argparse
+import logging
 import re
 from json.decoder import JSONDecodeError
 from urllib.parse import urlparse
 
 import requests
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def main():
@@ -14,14 +17,15 @@ def main():
     parser.add_argument("-i", "--id", type=str, metavar="SPACE_ID")
     args = parser.parse_args()
 
-    response = requests.get(f"https://twitter.com/i/spaces/{args.id}/peek").text
+    response = requests.get("https://twitter.com/").text
     last_line = response.splitlines()[-1]
     guest_token = re.findall(r"(?<=gt\=)\d{19}", last_line)[0]
+    logging.debug(guest_token)
 
     params = {
         "variables": '{"id":"'
         + args.id
-        + '","isMetatagsQuery":false,"withSuperFollowsUserFields":true,"withUserResults":true,"withBirdwatchPivots":false,"withReactionsMetadata":false,"withReactionsPerspective":false,"withSuperFollowsTweetFields":true,"withReplays":false,"withScheduledSpaces":true}'
+        + '","isMetatagsQuery":false,"withSuperFollowsUserFields":true,"withUserResults":true,"withBirdwatchPivots":false,"withReactionsMetadata":false,"withReactionsPerspective":false,"withSuperFollowsTweetFields":true,"withReplays":true,"withScheduledSpaces":true}'
     }
     headers = {
         "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs=1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
@@ -35,7 +39,9 @@ def main():
     metadata = response.json()
     try:
         media_key = metadata["data"]["audioSpace"]["metadata"]["media_key"]
+        logging.debug(media_key)
     except KeyError:
+        logging.error(metadata)
         raise RuntimeError(metadata)
 
     headers = {
@@ -51,6 +57,7 @@ def main():
     except JSONDecodeError:
         raise ValueError("Space has ended")
     dyn_url = metadata["source"]["location"]
+    logging.debug(dyn_url)
     master_url = dyn_url.removesuffix("?type=live").replace("dynamic", "master")
 
     response = requests.get(master_url)
@@ -60,9 +67,9 @@ def main():
     playlist_text = requests.get(playlist_url).text
     master_url_wo_file = master_url.removesuffix("master_playlist.m3u8")
     playlist_text = re.sub(r"(?=chunk)", master_url_wo_file, playlist_text)
-    with open(args.id + ".m3u8", "x") as stream_io:
+    with open(args.id + ".m3u8", "w") as stream_io:
         stream_io.write(playlist_text)
-    print(f"{args.id}.m3u8 written to disk")
+    logging.info(f"{args.id}.m3u8 written to disk")
 
 
 if __name__ == "__main__":
