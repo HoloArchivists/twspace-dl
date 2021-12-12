5a93e67bd4b94191431e7b53924b55b600d4bf57
Ryu18 <ryu@tpgjbo.xyz>
2021-12-12 15:07:08 +0100

    fix #21

--- twspace_dl/FormatInfo.py
+++ twspace_dl/FormatInfo.py
@@ -1,3 +1,6 @@
+"""twspace_dl formatting helper submodule"""
+import os
+import re
 from collections import defaultdict
 from datetime import datetime
 
@@ -9,7 +12,7 @@ class FormatInfo(dict):
     file name formatting
     """
 
-    DEFAULT_FNAME_FORMAT = "[%(creator_name)s]%(title)s-%(id)s"
+    DEFAULT_FNAME_FORMAT = "(%(creator_name)s)%(title)s-%(id)s"
 
     def __init__(self) -> None:
         dict.__init__(
@@ -36,12 +39,78 @@ class FormatInfo(dict):
             int(root["started_at"]) / 1000
         ).strftime("%Y-%m-%d")
 
+    # https://gist.github.com/dbr/256270
     @staticmethod
-    def sterilize_fn(filename: str) -> str:
-        bad_chars = '<>:"/\\|?*'
-        for char in bad_chars:
-            filename.replace(char, "_")
-        return filename
+    def sterilize_fn(value: str) -> str:
+        """
+        Takes a string and makes it into a valid filename.
+        """
+
+        # Treat extension seperatly
+        value, extension = os.path.splitext(value)
+
+        # Remove null byte
+        value = value.replace("\0", "")
+
+        # If the filename starts with a . prepend it with an underscore, so it
+        # doesn't become hidden
+        if value.startswith("."):
+            value = "_" + value
+
+        # platform.system docs say it could also return "Windows" or "Java".
+        # Failsafe and use Windows sanitisation for Java, as it could be any
+        # operating system.
+        blacklist = r"\/:*?\"<>|"
+
+        # Replace every blacklisted character with a underscore
+        value = re.sub("[%s]" % re.escape(blacklist), "_", value)
+
+        # Remove any trailing whitespace
+        value = value.strip()
+
+        # There are a bunch of filenames that are not allowed on Windows.
+        # As with character blacklist, treat non Darwin/Linux platforms as Windows
+        invalid_filenames = [
+            "CON",
+            "PRN",
+            "AUX",
+            "NUL",
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "LPT1",
+            "LPT2",
+            "LPT3",
+            "LPT4",
+            "LPT5",
+            "LPT6",
+            "LPT7",
+            "LPT8",
+            "LPT9",
+        ]
+        if value in invalid_filenames:
+            value = "_" + value
+
+        # Truncate filenames to valid length
+        max_len = 32
+
+        if len(value + extension) > max_len:
+            if len(extension) > len(value):
+                # Truncate extension instead of filename, no extension should be
+                # this long..
+                new_length = max_len - len(value)
+                extension = extension[:new_length]
+            else:
+                new_length = max_len - len(extension)
+                value = value[:new_length]
+
+        return value + extension
 
     def format(self, format_str: str) -> str:
         return format_str % self
