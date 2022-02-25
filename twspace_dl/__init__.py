aac1d72a94ef3e021d1e1158e4c2eebe2b91d550
Ryu1845 <ryu@tpgjbo.xyz>
2022-02-26 00:37:35 +0100

    update

--- twspace_dl/__init__.py
+++ twspace_dl/__init__.py
@@ -1,12 +1,11 @@
 from .twspace_dl import TwspaceDL
 from .twspace import Twspace
-from .twitter import user_id, guest_token
+from . import twitter
 from .login import Login, load_from_file, write_to_file
 
 __all__ = [
     "Twspace",
-    "user_id",
-    "guest_token",
+    "twitter",
     "TwspaceDL",
     "Login",
     "load_from_file",
