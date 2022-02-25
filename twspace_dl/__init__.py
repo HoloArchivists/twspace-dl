e5ad9dd428e2ded1ddb59415262b5cf028ad0641
Ryu1845 <ryu@tpgjbo.xyz>
2022-02-26 00:23:13 +0100

    expose user_id and guest_token from module

--- twspace_dl/__init__.py
+++ twspace_dl/__init__.py
@@ -1,5 +1,14 @@
 from .twspace_dl import TwspaceDL
 from .twspace import Twspace
+from .twitter import user_id, guest_token
 from .login import Login, load_from_file, write_to_file
 
-__all__ = ["Twspace", "TwspaceDL", "Login", "load_from_file", "write_to_file"]
+__all__ = [
+    "Twspace",
+    "user_id",
+    "guest_token",
+    "TwspaceDL",
+    "Login",
+    "load_from_file",
+    "write_to_file",
+]
