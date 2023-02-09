28aae9897abac559da587bfcabd4ee922f5b554d
Mike Lei <mikelei8291@users.noreply.github.com>
2023-02-09 00:22:02 +0000

    fix(twitter): use cached `guest_token` to mitigate API rate limit

--- twspace_dl/twitter.py
+++ twspace_dl/twitter.py
@@ -1,6 +1,10 @@
 import re
+import tempfile
+import time
+from os.path import getmtime, join
 
 import requests
+
 
 AUTH_HEADER = {
     "authorization": (
@@ -9,19 +13,32 @@ AUTH_HEADER = {
         "=1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
     )
 }
+GUEST_TOKEN = None
+GUEST_TOKEN_FILE = join(tempfile.gettempdir(), "twspace_dl-guest_token")
+GUEST_TOKEN_TIMEOUT = 1800
 
 
-def guest_token() -> str:
+def guest_token(refresh: bool = False) -> str:
     """Generate a guest token to authorize twitter api requests"""
-    response = requests.post(
-        "https://api.twitter.com/1.1/guest/activate.json",
-        headers=AUTH_HEADER,
-        timeout=30,
-    ).json()
-    token = response["guest_token"]
-    if not token:
-        raise RuntimeError("No guest token found after five retry")
-    return token
+    global GUEST_TOKEN
+    if not GUEST_TOKEN or refresh:
+        try:
+            if refresh or time.time() - getmtime(GUEST_TOKEN_FILE) > GUEST_TOKEN_TIMEOUT:
+                raise FileNotFoundError
+            with open(GUEST_TOKEN_FILE) as f:
+                GUEST_TOKEN = f.read()
+        except FileNotFoundError:
+            response = requests.post(
+                "https://api.twitter.com/1.1/guest/activate.json",
+                headers=AUTH_HEADER,
+                timeout=30,
+            ).json()
+            if GUEST_TOKEN := response["guest_token"]:
+                with open(GUEST_TOKEN_FILE, "w") as f:
+                    f.write(GUEST_TOKEN)
+            else:
+                raise RuntimeError("No guest token found after five retry")
+    return GUEST_TOKEN
 
 
 def user_id(user_url: str) -> str:
@@ -43,11 +60,16 @@ def user_id(user_url: str) -> str:
             "}"
         ),
     }
-    response = requests.get(
+    session = requests.Session()
+    req = requests.Request(
+        "GET",
         "https://api.twitter.com/graphql/hVhfo_TquFTmgL7gYwf91Q/UserByScreenName",
         params=params,
-        headers={**AUTH_HEADER, "x-guest-token": guest_token()},
-        timeout=30,
-    ).json()
-    usr_id = response["data"]["user"]["result"]["rest_id"]
+        headers={**AUTH_HEADER, "x-guest-token": guest_token()}
+    ).prepare()
+    response = session.send(req, timeout=30)
+    if response.status_code == requests.codes.too_many_requests:
+        req.headers.update({"x-guest-token": guest_token(True)})
+        response = session.send(req)
+    usr_id = response.json()["data"]["user"]["result"]["rest_id"]
     return usr_id
