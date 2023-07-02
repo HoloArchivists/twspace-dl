fb3761a573d94d1f8b3056c56b5ab4611b1aeb2c
Mike Lei <mikelei8291@users.noreply.github.com>
2023-07-03 06:34:54 +0800

    doc(api, cookies): add documentation

--- twspace_dl/cookies.py
+++ twspace_dl/cookies.py
@@ -4,6 +4,13 @@ from .api import REQUIRED_COOKIES
 
 
 class CookiesLoader:
+    """Loader of cookies files.
+
+    Compatible with cookies file in Netscape format.
+    More details of the format: https://curl.se/docs/http-cookies.html
+    """
+
+    """The regex pattern to extract keys and values of all required cookies."""
     COOKIES_PATTERN = re.compile(
         r"\s+({keys})\s+(\w+)$".format(keys="|".join(REQUIRED_COOKIES)),
         re.MULTILINE
@@ -14,6 +21,14 @@ class CookiesLoader:
 
     @classmethod
     def load(cls, path: str) -> dict[str, str]:
+        """Load cookies from the specified path.
+
+        - path: The path to the cookies file.
+
+        - return: A `dict` of keys and values of all required cookies.
+
+        - raise RuntimeError: If the cookies file failed to load.
+        """
         try:
             with open(path, encoding="utf-8") as f:
                 return dict(cls.COOKIES_PATTERN.findall(f.read()))
